e7e6d150e6b6f434a25ea411dda652c4dc8c5b72
Felix Peters <peters@develop4edu.de>
2015-03-03 21:52:58 +0100

    refactor services to xml files and use a Extension to load

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AppExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $config An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('forms.xml');
+        $loader->load('security.xml');
+    }
+}
---
+++ src/AppBundle/Tests/DependencyInjection/AppExtensionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Tests\DependencyInjection;
+
+use AppBundle\DependencyInjection\AppExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+class AppExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * Return an array of container extensions you need to be registered for each test (usually just the container
+     * extension you are testing.
+     *
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return array(new AppExtension());
+    }
+
+    /**
+     * Test if all service configuration files are loaded
+     */
+    public function testIsLoadableAndIncludeAllFiles()
+    {
+        $this->load();
+        // security.xml
+        $this->assertContainerBuilderHasService('security.access.team_voter');
+        // forms.xml
+        $this->assertContainerBuilderHasService('app.registration.form.type');
+    }
+}
