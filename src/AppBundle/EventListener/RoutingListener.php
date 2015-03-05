039915272a234d5d4bfbbbb98fe54eb3f00ecbcf
Mewes Kochheim <mewes@kochheim.de>
2015-03-05 23:34:30 +0100

    Added listener logic to handle special routing rules

--- src/AppBundle/EventListener/RoutingListener.php
+++ src/AppBundle/EventListener/RoutingListener.php
@@ -2,17 +2,50 @@
 
 namespace AppBundle\EventListener;
 
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class RoutingListener
 {
+    /**
+     * @var AuthorizationChecker
+     */
+    private $authorizationChecker;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct(AuthorizationChecker $authorizationChecker,  Router $router)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->router = $router;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
         if (!$event->isMasterRequest()) {
             return;
         }
 
-        dump($event);
+        if ($this->authorizationChecker->isGranted('ROLE_USER') && in_array($event->getRequest()->get('_route'), [
+            'fos_user_security_login',
+            'fos_user_registration_register',
+            'fos_user_registration_check_email',
+            'fos_user_registration_confirm',
+            'fos_user_registration_confirmed',
+            'fos_user_resetting_request ',
+            'fos_user_resetting_send_email',
+            'fos_user_resetting_check_email',
+            'fos_user_resetting_reset',
+            'fos_user_change_password'
+        ])) {
+            $event->setResponse(RedirectResponse::create($this->router->generate('dashboard_index')));
+        }
     }
 }
