628475ff9b1bbc5ac1cfd9410b471679233155ab
Mewes Kochheim <mewes@kochheim.de>
2015-03-06 00:35:52 +0100

    Made FosUser forms play along nicer

--- src/AppBundle/Form/Type/ProfileFormType.php
+++ src/AppBundle/Form/Type/ProfileFormType.php
@@ -4,12 +4,22 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class ProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('memberships')->add('activeTeam');
+        $builder
+            ->remove('current_password')
+            ->add('memberships')
+            ->add('activeTeam')
+            ->add('current_password', 'password', [
+                'label' => 'form.current_password',
+                'translation_domain' => 'FOSUserBundle',
+                'mapped' => false,
+                'constraints' => new UserPassword()
+            ]);
     }
 
     public function getParent()
