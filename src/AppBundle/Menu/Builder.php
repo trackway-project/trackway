b293ef019c9dddb54eac411e0eea6be06277e03c
Mewes Kochheim <mewes@kochheim.de>
2015-03-05 22:17:02 +0100

    Added basic route parameter handling to the menu builder

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -7,6 +7,7 @@ use Knp\Menu\FactoryInterface;
 use Knp\Menu\MenuItem;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
@@ -20,12 +21,19 @@ class Builder extends ContainerAware
         $authorizationChecker = $this->container->get('security.authorization_checker');
 
         if ($authorizationChecker->isGranted('ROLE_USER')) {
+            /** @var Request $request */
+            $request = $this->container->get('request');
+            $route = $request->get('_route');
+            $id = $request->get('id');
+
             $menu->addChild('Teams', ['route' => 'team_index']);
             $menu['Teams']->addChild('Overview', ['route' => 'team_index']);
             $menu['Teams']->addChild('Create', ['route' => 'team_new']);
-            $menu['Teams']->addChild('Show', ['route' => 'team_show', 'routeParameters' => ['id' => 0]]);
-            $menu['Teams']->addChild('Edit', ['route' => 'team_edit', 'routeParameters' => ['id' => 0]]);
-            $menu['Teams']->addChild('Delete', ['route' => 'team_delete', 'routeParameters' => ['id' => 0]]);
+            if (strpos($route, 'team_') === 0 && $id) {
+                $menu['Teams']->addChild('Show', ['route' => 'team_show', 'routeParameters' => ['id' => $id]]);
+                $menu['Teams']->addChild('Edit', ['route' => 'team_edit', 'routeParameters' => ['id' => $id]]);
+                $menu['Teams']->addChild('Delete', ['route' => 'team_delete', 'routeParameters' => ['id' => $id]]);
+            }
 
             $activeTeam = $this->container->get('security.token_storage')->getToken()->getUser()->getActiveTeam();
 
@@ -37,10 +45,12 @@ class Builder extends ContainerAware
                 if ($isTeamAdmin) {
                     $menu['Projects']->addChild('Create', ['route' => 'project_new']);
                 }
-                $menu['Projects']->addChild('Show', ['route' => 'project_show', 'routeParameters' => ['id' => 0]]);
-                if ($isTeamAdmin) {
-                    $menu['Projects']->addChild('Edit', ['route' => 'project_edit', 'routeParameters' => ['id' => 0]]);
-                    $menu['Projects']->addChild('Delete', ['route' => 'project_delete', 'routeParameters' => ['id' => 0]]);
+                if (strpos($route, 'project_') === 0 && $id) {
+                    $menu['Projects']->addChild('Show', ['route' => 'project_show', 'routeParameters' => ['id' => $id]]);
+                    if ($isTeamAdmin) {
+                        $menu['Projects']->addChild('Edit', ['route' => 'project_edit', 'routeParameters' => ['id' => $id]]);
+                        $menu['Projects']->addChild('Delete', ['route' => 'project_delete', 'routeParameters' => ['id' => $id]]);
+                    }
                 }
 
                 $menu->addChild('Tasks', ['route' => 'task_index']);
@@ -48,25 +58,31 @@ class Builder extends ContainerAware
                 if ($isTeamAdmin) {
                     $menu['Tasks']->addChild('Create', ['route' => 'task_new']);
                 }
-                $menu['Tasks']->addChild('Show', ['route' => 'task_show', 'routeParameters' => ['id' => 0]]);
-                if ($isTeamAdmin) {
-                    $menu['Tasks']->addChild('Edit', ['route' => 'task_edit', 'routeParameters' => ['id' => 0]]);
-                    $menu['Tasks']->addChild('Delete', ['route' => 'task_delete', 'routeParameters' => ['id' => 0]]);
+                if (strpos($route, 'task_') === 0 && $id) {
+                    $menu['Tasks']->addChild('Show', ['route' => 'task_show', 'routeParameters' => ['id' => $id]]);
+                    if ($isTeamAdmin) {
+                        $menu['Tasks']->addChild('Edit', ['route' => 'task_edit', 'routeParameters' => ['id' => $id]]);
+                        $menu['Tasks']->addChild('Delete', ['route' => 'task_delete', 'routeParameters' => ['id' => $id]]);
+                    }
                 }
 
                 $menu->addChild('Time Entries', ['route' => 'timeentry_index']);
                 $menu['Time Entries']->addChild('Overview', ['route' => 'timeentry_index']);
                 $menu['Time Entries']->addChild('Create', ['route' => 'timeentry_new']);
-                $menu['Time Entries']->addChild('Show', ['route' => 'timeentry_show', 'routeParameters' => ['id' => 0]]);
-                $menu['Time Entries']->addChild('Edit', ['route' => 'timeentry_edit', 'routeParameters' => ['id' => 0]]);
-                $menu['Time Entries']->addChild('Delete', ['route' => 'timeentry_delete', 'routeParameters' => ['id' => 0]]);
+                if (strpos($route, 'timeentry_') === 0 && $id) {
+                    $menu['Time Entries']->addChild('Show', ['route' => 'timeentry_show', 'routeParameters' => ['id' => $id]]);
+                    $menu['Time Entries']->addChild('Edit', ['route' => 'timeentry_edit', 'routeParameters' => ['id' => $id]]);
+                    $menu['Time Entries']->addChild('Delete', ['route' => 'timeentry_delete', 'routeParameters' => ['id' => $id]]);
+                }
 
                 $menu->addChild('Absences', ['route' => 'absence_index']);
                 $menu['Absences']->addChild('Overview', ['route' => 'absence_index']);
                 $menu['Absences']->addChild('Create', ['route' => 'absence_new']);
-                $menu['Absences']->addChild('Show', ['route' => 'absence_show', 'routeParameters' => ['id' => 0]]);
-                $menu['Absences']->addChild('Edit', ['route' => 'absence_edit', 'routeParameters' => ['id' => 0]]);
-                $menu['Absences']->addChild('Delete', ['route' => 'absence_delete', 'routeParameters' => ['id' => 0]]);
+                if (strpos($route, 'absence_') === 0 && $id) {
+                    $menu['Absences']->addChild('Show', ['route' => 'absence_show', 'routeParameters' => ['id' => $id]]);
+                    $menu['Absences']->addChild('Edit', ['route' => 'absence_edit', 'routeParameters' => ['id' => $id]]);
+                    $menu['Absences']->addChild('Delete', ['route' => 'absence_delete', 'routeParameters' => ['id' => $id]]);
+                }
             }
         }
 
