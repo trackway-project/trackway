3bc8b4c15f87bd2fefcfa8b07ea63877bfdf8a5f
Mewes Kochheim <mewes@kochheim.de>
2015-03-07 22:45:36 +0100

    Removed absenceReasons.sql

--- absencereasons.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `absenceReasons` (`id`,`name`) VALUES (1,'Illness');
-INSERT INTO `absenceReasons` (`id`,`name`) VALUES (2,'Vacation');
-INSERT INTO `absenceReasons` (`id`,`name`) VALUES (3,'Holiday');
