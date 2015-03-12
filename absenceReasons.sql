8389805dc21d715c0bf4ec340fbabc37c186cf16
Mewes Kochheim <mewes@kochheim.de>
2015-03-13 00:54:06 +0100

    Readded SQL files. Still needed for Travis-CI!!! Could be replaced by a Symfony console command to initialize the DB.

---
+++ absenceReasons.sql
@@ -0,0 +1,3 @@
+INSERT INTO `absenceReasons` (`id`,`name`) VALUES (1,'Illness');
+INSERT INTO `absenceReasons` (`id`,`name`) VALUES (2,'Vacation');
+INSERT INTO `absenceReasons` (`id`,`name`) VALUES (3,'Holiday');
---
+++ groups.sql
@@ -0,0 +1,3 @@
+INSERT INTO `groups` (`id`,`name`,`roles`) VALUES (1,'Owner','a:1:{i:0;s:10:\"ROLE_ADMIN\";}');
+INSERT INTO `groups` (`id`,`name`,`roles`) VALUES (2,'Admin','a:1:{i:0;s:10:\"ROLE_ADMIN\";}');
+INSERT INTO `groups` (`id`,`name`,`roles`) VALUES (3,'User','a:1:{i:0;s:9:\"ROLE_USER\";}');
